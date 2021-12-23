b7ce2931b3d421c8f3261fe1a59f5259bff3fcd8
AhmedHamdyy99 <85260561+AhmedHamdyy99@users.noreply.github.com>
2021-12-23 23:59:02 +0200

    Add files via upload

---
+++ xmlcutter.h
@@ -0,0 +1,11 @@
+#ifndef XMLCUTTER_H
+#define XMLCUTTER_H
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> xml_cutter(string filepath);
+#endif
