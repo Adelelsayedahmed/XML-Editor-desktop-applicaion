1e6149ce1f468a1c834a3526b563a7703dd5202c
AhmedHamdyy99 <85260561+AhmedHamdyy99@users.noreply.github.com>
2022-02-08 21:25:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include "mainwindow.h"
-
+#include "GRAPH.h"
+#include "XMLTree.h"
+#include <ostream>
 #include <QApplication>
 
-int main(int argc, char *argv[])
+
+using namespace std;
+
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
+}
+
+void Graphfile(SocialGraph& Graph){
+
+      ofstream myfile("graph.dot");
+//    string Colors[] = {"[color=blue]\n","[color=red]\n","[color=green]\n"};
+
+      myfile<<"digraph{\n";
+      myfile<<"bgcolor=\"lightyellow\"\n";
+
+      for(int i =1; i<Graph.usersSocialNodes.size(); i++){
+          SocialNode* user = Graph.usersSocialNodes[i];
+                  myfile << user->id ; // << Colors[i-1];
+                  myfile << user->id<<"-> {";
+            for(int k = 0; k<user->followers.size(); k++){
+                myfile<<user->followers[k]<<" ";
+            }
+            myfile<<"}\n";
+      }
+      myfile<<"}";
+
+      myfile.close();
 }
